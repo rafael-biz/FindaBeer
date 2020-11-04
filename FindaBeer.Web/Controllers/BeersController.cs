c67f23e47f6125f265ea0fd8e020063198ac2791
Rafael Biz <rafael@seventh.com.br>
2020-11-04 20:56:18 -0300

    Add project files.

---
+++ FindaBeer.Web/Controllers/BeersController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FindaBeer.Api.Controllers
+{
+    [Route("api/beers")]
+    [ApiController]
+    public class BeersController : ControllerBase
+    {
+        /// <summary>
+        /// Retorna uma lista de cervejas.
+        /// </summary>
+        [HttpGet]
+        public IEnumerable<BeerDTO> Get()
+        {
+            var list = new List<BeerDTO>()
+            {
+                new BeerDTO()
+                {
+                    Id = 1,
+                    Name = "Antartica"
+                },
+                new BeerDTO()
+                {
+                    Id = 2,
+                    Name = "Skol"
+                },
+                new BeerDTO()
+                {
+                    Id = 3,
+                    Name = "Colorado"
+                },
+            };
+
+            return list;
+        }
+    }
+
+    public class BeerDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ FindaBeer.Web/Startup.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace FindaBeer.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "FindaBeer API",
+                    Description = "Encontre uma cerveja com essa poderosa API =P"
+                });
+
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, $"FindaBeer.Web.xml");
+
+                c.IncludeXmlComments(xmlPath);
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path.Value.Equals("/"))
+                {
+                    context.Request.Path = "/index.html";
+                }
+                await next();
+            });
+            
+            DefaultFilesOptions options = new DefaultFilesOptions();
+            options.DefaultFileNames.Clear();
+            options.DefaultFileNames.Add("index.html");
+
+            app.UseDefaultFiles(options);
+            app.UseStaticFiles();
+            
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "FindaBeer API");
+                c.RoutePrefix = "api/help";
+            });
+        }
+    }
+}
